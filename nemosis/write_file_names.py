e311a96529b1e38360494c571929a2c223f11e3b
prakaa <abiprakash007@gmail.com>
2020-02-12 17:30:45 +1100

    fix case of .csv so downloaded files are recognised

--- nemosis/write_file_names.py
+++ nemosis/write_file_names.py
@@ -3,23 +3,23 @@ from nemosis import defaults
 
 def write_file_names(name, month, year, day, index, raw_data_location):
     # Add the year and month information to the generic AEMO file name
-    filename_full = defaults.names[name] + "_" + str(year) + str(month) + "010000.CSV"
+    filename_full = defaults.names[name] + "_" + str(year) + str(month) + "010000.csv"
     # Check if file already exists in the raw data folder.
-    path_and_name = raw_data_location + '/' + filename_full
+    path_and_name = raw_data_location + os.sep + filename_full
     # Add the year and month information to the generic AEMO file name
     filename_full_feather = defaults.names[name] + "_" + str(year) + str(month) + "010000.feather"
     # Check if file already exists in the raw data folder.
-    path_and_name_feather = raw_data_location + '/' + filename_full_feather
+    path_and_name_feather = raw_data_location + os.sep + filename_full_feather
     return filename_full, path_and_name, filename_full_feather, path_and_name_feather
 
 
 def write_file_names_fcas(name, month, year, day, index, raw_data_location):
     # Add the year and month information to the generic AEMO file name
-    filename_full = defaults.names[name] + "_" + str(year) + str(month) + day + index + ".CSV"
+    filename_full = defaults.names[name] + "_" + str(year) + str(month) + day + index + ".csv"
     # Check if file already exists in the raw data folder.
-    path_and_name = raw_data_location + '/' + filename_full
+    path_and_name = raw_data_location + os.sep + filename_full
     # Add the year and month information to the generic AEMO file name
     filename_full_feather = defaults.names[name] + "_" + str(year) + str(month) + day + index + ".feather"
     # Check if file already exists in the raw data folder.
-    path_and_name_feather = raw_data_location + '/' + filename_full_feather
+    path_and_name_feather = raw_data_location + os.sep + filename_full_feather
     return filename_full, path_and_name, filename_full_feather, path_and_name_feather
