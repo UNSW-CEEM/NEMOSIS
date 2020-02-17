11ea67402c3edf15eafbdf0b789fb731a5c338ce
prakaa <abiprakash007@gmail.com>
2020-02-17 15:33:20 +1100

    modify fetch methods to ouput multiple formats and clean up downloader exception handling

--- nemosis/downloader.py
+++ nemosis/downloader.py
@@ -1,78 +1,93 @@
+import os
 import requests
 import zipfile
 import io
 from nemosis import defaults
 
 # Windows Chrome for User-Agent request headers
-USR_AGENT_HEADER = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36'}
+USR_AGENT_HEADER = {'User-Agent': ('Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+                                   + ' AppleWebKit/537.36 (KHTML, like Gecko) '
+                                   + 'Chrome/80.0.3987.87 Safari/537.36')}
 
-def run(year, month, day, index, filename, down_load_to):
+# warning for HTTP 400s
+warning_400 = ('Data location may have moved,'
+               + ' or additional authentication is required')
+
+
+def run(year, month, day, index, filename_stub, down_load_to):
     """This function"""
 
     url = defaults.aemo_data_url
     # Add the year and month information to the generic AEMO data url
-    url_formatted = format_aemo_url(url, year, month, filename)
+    url_formatted = format_aemo_url(url, year, month, filename_stub)
 
     # Perform the download, unzipping saving of the file
     try:
-        download_unzip_csv(url_formatted, down_load_to, filename)
-    except:
-        print('Warning {} not downloaded'.format(filename))
+        status_code = download_unzip_csv(url_formatted, down_load_to)
+    except Exception:
+        print('Warning {} not downloaded'.format(filename_stub))
+        raise
+    finally:
+        status_code = status_code_return(url_formatted)
+        if status_code >= 400 and status_code < 500:
+            raise requests.exceptions.HTTPError(f'HTTP {status_code}.'
+                                                + '{warning_400}')
 
 
-def run_fcas4s(year, month, day, index, filename, down_load_to):
+def run_fcas4s(year, month, day, index, filename_stub, down_load_to):
     """This function"""
 
     # Add the year and month information to the generic AEMO data url
     url_formatted_latest = defaults.fcas_4_url.format(year, month, day, index)
-    url_formatted_hist = defaults.fcas_4_url_hist.format(year, year, month, year, month, day, index)
-
-
+    url_formatted_hist = defaults.fcas_4_url_hist.format(year, year, month,
+                                                         year, month, day,
+                                                         index)
     # Perform the download, unzipping saving of the file
     try:
-        download_unzip_csv(url_formatted_latest, down_load_to, filename)
-    except:
+        download_unzip_csv(url_formatted_latest, down_load_to)
+    except Exception:
         try:
-            download_unzip_csv(url_formatted_hist, down_load_to, filename)
-        except:
-            print('Warning {} not downloaded'.format(filename))
-
-
-def download_unzip_csv(url, down_load_to, filename):
-    """This function downloads a zipped csv using a url, extracts the csv and saves it a specified location and with
-    a specified filename"""
-    try:
-        r = requests.get(url, headers=USR_AGENT_HEADER)
-    except requests.exceptions.RequestException as e:
-        print(e)
-        raise
+            download_unzip_csv(url_formatted_hist, down_load_to)
+        except Exception:
+            # FCAS csvs are bundled in 30 minute bundles
+            # Check if the csv exists before warning
+            file_check = os.path.join(down_load_to, filename_stub + '.csv')
+            if not os.path.isfile(file_check):
+                print('Warning {} not downloaded'.format(filename_stub))
     finally:
-        if r.status_code >= 400 and r.status_code < 500:
-            raise requests.exceptions.HTTPError(f'HTTP {r.status_code}. Data location may have moved, or additional authentication is required')
+        status_codes = [status_code_return(url_formatted_latest),
+                        status_code_return(url_formatted_hist)]
+        http_codes = [x for x in status_codes if x >= 400 and x < 400]
+        if http_codes:
+            raise requests.exceptions.HTTPError(f'HTTP {http_codes}.'
+                                                + '{warning_400}')
 
+
+def download_unzip_csv(url, down_load_to):
+    """
+    This function downloads a zipped csv using a url,
+    extracts the csv and saves it a specified location
+    """
+    r = requests.get(url, headers=USR_AGENT_HEADER)
     z = zipfile.ZipFile(io.BytesIO(r.content))
     z.extractall(down_load_to)
 
 
 def download_csv(url, down_load_to, path_and_name):
-    """This function downloads a zipped csv using a url, extracts the csv and saves it a specified location and with
-    a specified filename"""
-    try:
-        r = requests.get(url, headers=USR_AGENT_HEADER)
-    except requests.exceptions.RequestException as e:
-        print(e)
-        raise
-    finally:
-        if r.status_code >= 400 and r.status_code < 500:
-            raise requests.exceptions.HTTPError(f'HTTP {r.status_code}. Data location may have moved, or additional authentication is required')
-
+    """
+    This function downloads a zipped csv using a url,
+    extracts the csv and saves it a specified location
+    """
+    r = requests.get(url, headers=USR_AGENT_HEADER)
     with open(path_and_name, 'wb') as f:
         f.write(r.content)
 
 
 def download_xl(url, down_load_to, path_and_name):
-    """This function downloads a zipped csv using a url, extracts the csv and saves it a specified location and with
-    a specified filename"""
+    """
+    This function downloads a zipped csv using a url, extracts the csv and
+    saves it a specified location
+    """
     try:
         r = requests.get(url, headers=USR_AGENT_HEADER)
     except requests.exceptions.RequestException as e:
@@ -80,13 +95,23 @@ def download_xl(url, down_load_to, path_and_name):
         raise
     finally:
         if r.status_code >= 400 and r.status_code < 500:
-            raise requests.exceptions.HTTPError(f'HTTP {r.status_code}. Data location may have moved, or additional authentication is required')
+            raise requests.exceptions.HTTPError(f'HTTP {r.status_code}. '
+                                                + '{warning_400}')
 
     with open(path_and_name, 'wb') as f:
         f.write(r.content)
 
-def format_aemo_url(url, year, month, filename):
-    """This fills in the missing information in the AEMO url so data for the right month, year and file name are
-    downloaded"""
+
+def format_aemo_url(url, year, month, filename_stub):
+    """
+    This fills in the missing information in the AEMO URL
+    so data for the right month, year and file name are
+    downloaded
+    """
     year = str(year)
-    return url.format(year, year, month, filename[:-4])
+    return url.format(year, year, month, filename_stub)
+
+
+def status_code_return(url):
+    r = requests.get(url, headers=USR_AGENT_HEADER)
+    return r.status_code
--- nemosis/write_file_names.py
+++ nemosis/write_file_names.py
@@ -1,25 +1,18 @@
 import os
 from nemosis import defaults
 
+
 def write_file_names(name, month, year, day, index, raw_data_location):
     # Add the year and month information to the generic AEMO file name
-    filename_full = defaults.names[name] + "_" + str(year) + str(month) + "010000.CSV"
-    # Check if file already exists in the raw data folder.
-    path_and_name = raw_data_location + '/' + filename_full
-    # Add the year and month information to the generic AEMO file name
-    filename_full_feather = defaults.names[name] + "_" + str(year) + str(month) + "010000.feather"
-    # Check if file already exists in the raw data folder.
-    path_and_name_feather = raw_data_location + '/' + filename_full_feather
-    return filename_full, path_and_name, filename_full_feather, path_and_name_feather
+    filename_stub = (defaults.names[name] + "_" + str(year)
+                     + str(month) + "010000")
+    path_and_name = os.path.join(raw_data_location, filename_stub)
+    return filename_stub, path_and_name
 
 
 def write_file_names_fcas(name, month, year, day, index, raw_data_location):
     # Add the year and month information to the generic AEMO file name
-    filename_full = defaults.names[name] + "_" + str(year) + str(month) + day + index + ".CSV"
-    # Check if file already exists in the raw data folder.
-    path_and_name = raw_data_location + '/' + filename_full
-    # Add the year and month information to the generic AEMO file name
-    filename_full_feather = defaults.names[name] + "_" + str(year) + str(month) + day + index + ".feather"
-    # Check if file already exists in the raw data folder.
-    path_and_name_feather = raw_data_location + '/' + filename_full_feather
-    return filename_full, path_and_name, filename_full_feather, path_and_name_feather
+    filename_stub = (defaults.names[name] + "_" + str(year)
+                     + str(month) + day + index)
+    path_and_name = os.path.join(raw_data_location, filename_stub)
+    return filename_stub, path_and_name
