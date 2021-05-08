a71e1785ab2fb9068abebb2a08284fbd3e76dbb3
abiprakash007@gmail.com <abiprakash007@gmail.com>
2021-05-08 21:50:18 +1000

    update import from pandas - deprecation warning

--- nemosis/test_query_wrapers.py
+++ nemosis/test_query_wrapers.py
@@ -1,7 +1,7 @@
 import unittest
 import pandas as pd
 from nemosis import query_wrapers
-from pandas.util.testing import assert_frame_equal
+from pandas.testing import assert_frame_equal
 from datetime import datetime
 from nemosis import defaults
 
