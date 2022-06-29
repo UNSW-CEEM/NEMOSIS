085db726a7ae7996402fc7fb9fd804d040547f27
nick-gorman <40549624+nick-gorman@users.noreply.github.com>
2022-06-29 12:43:17 +1000

    correct version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="nemosis",
-    version="3.0.1",
+    version="3.1.0",
     author="Nicholas Gorman, Abhijith Prakash",
     author_email="n.gorman305@gmail.com",
     description="A tool for accessing AEMO data.",
