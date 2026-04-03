7e71e73c9d6d7f5dc4025f113308c4ddcc9a3756
Matthew Davis <github@mdavis.xyz>
2026-04-03 09:36:52 +0200

    More robust checks and error messages for unset cache path

---
+++ tests/test_00_test_setup.py
@@ -0,0 +1,10 @@
+# This file is named 00 so that it runs first
+# to help debug, because it this test fails, all others will fail
+
+import unittest
+
+from nemosis import defaults
+
+class TestDefault(unittest.TestCase):
+    def test_cache_path(self):
+        self.assertIsNotNone(raw_data_cache, "raw_data_cache not set. Try setting NEMOSIS_TEST_CACHE env var.")
